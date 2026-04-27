79a33e6858fdb01398ceace7a77194309a8ac7d7
billybagginz <billybagginz@gmail.com>
2026-04-27 08:59:25 -0600

    chore: bump android version to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.bagginzventures.countdownwidget"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.1.2"
+        versionCode = 6
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
